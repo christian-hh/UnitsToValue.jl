56793a565be461719fe471448f9ccfe04ca0decf
Christian Hallas <hallaschristian@gmail.com>
2022-09-29 23:06:59 -0400

    Initial version

--- src/UnitsToValue.jl
+++ src/UnitsToValue.jl
@@ -1,5 +1,18 @@
 module UnitsToValue
 
-# Write your package code here.
+using Unitful
+using PhysicalConstants.CODATA2018
+
+macro with_unit(arg1, arg2)
+    arg2 = @eval @u_str $arg2
+    return convert(Float64, upreferred(eval(arg1) .* arg2).val)
+end
+export @with_unit
+
+const c     = SpeedOfLightInVacuum
+const gS    = 2.0023193043617
+const μX    = 1e-6 * 1.465 * (1e-21 / c) / h; # for CaOH, in MHz
+
+export c, gS, μ_B, h, ħ, ε_0, μ_0, μX
 
 end
