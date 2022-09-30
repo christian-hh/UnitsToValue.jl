6aa00a9fd852e14e1371c3b7666d8e1bbce107e6
Christian Hallas <hallaschristian@gmail.com>
2022-09-29 23:35:28 -0400

    ...

--- src/UnitsToValue.jl
+++ src/UnitsToValue.jl
@@ -1,7 +1,7 @@
 module UnitsToValue
 
 using Unitful
-using PhysicalConstants.CODATA2018
+using PhysicalConstants
 
 macro with_unit(arg1, arg2)
     arg2 = @eval @u_str $arg2
@@ -9,10 +9,16 @@ macro with_unit(arg1, arg2)
 end
 export @with_unit
 
-const c     = SpeedOfLightInVacuum
 const gS    = 2.0023193043617
-const μX    = 1e-6 * 1.465 * (1e-21 / c) / h; # for CaOH, in MHz
+# const μX    = 1e-6 * 1.465 * (1e-21 / c) / h; # for CaOH, in MHz
 
-export c, gS, μ_B, h, ħ, ε_0, μ_0, μX
+const c     = CODATA2018.SpeedOfLightInVacuum.val
+const h     = CODATA2018.h.val
+const ħ     = CODATA2018.ħ.val
+const μ_B   = CODATA2018.μ_B.val
+const ε_0   = CODATA2018.ε_0.val
+const μ_0   = CODATA2018.μ_0.val
+
+export c, gS, μ_B, h, ħ, ε_0, μ_0#, μX
 
 end
